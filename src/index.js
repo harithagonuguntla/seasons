f8a5209b8f6c018f04d25f057b3f167b91459f3b
Haritha Gonuguntla <Haritha@Harithas-MacBook-Pro.local>
2020-09-08 12:33:29 -0500

    changed index.js, added spinner.js files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
 
@@ -12,20 +13,27 @@ class App extends React.Component {
             err => this.setState({ errorMessage: err.message })
         )
     }
-    
-    render() {
-       
+
+    renderContent() {
         if(this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div> 
         }
 
         if(!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
 
-        return <div>Loading!</div>
-                
+        return <div><Spinner message = "Please accept location request"/></div>
     }
+    
+    
+    render() {
+       return (
+           <div className = "border red">
+               {this.renderContent()}
+           </div>
+       )
+    }      
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
